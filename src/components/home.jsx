dff6b9fc0b20c9f33274c6600661281beb792284
darpan9460@gmail.com <darpan9460@gmail.com>
2022-03-25 16:53:27 +0530

    added history

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,7 +11,7 @@ function Home() {
       <Slider />
       <History />
       <Heritage />
-      <Story />
+      {/* <Story /> */}
       <Cuisine />
       <Epic />
     </div>
--- src/components/segments/Epics/epics.js
+++ src/components/segments/Epics/epics.js
@@ -2,9 +2,9 @@ const epics=[
     {
         id:1,
         name:"Mahabharat",
-        img:"/Imgs/segments/heritage/monuments/monumentsLogo.jpeg" ,
+        img:"/Imgs/segments/epics/mahabharata/maha.jpg" ,
         description: {__html: "Mahabharata, (Sanskrit: “Great Epic of the Bharata Dynasty”) one of the two Sanskrit epic poems of ancient India (the other being the Ramayana). The Mahabharata is an important source of information on the development of Hinduism between 400 BCE and 200 CE and is regarded by Hindus as both a text about dharma (Hindu moral law) and a history (itihasa, literally “that’s what happened”). Appearing in its present form about 400 CE, the Mahabharata consists of a mass of mythological and didactic material arranged around a central heroic narrative that tells of the struggle for sovereignty between two groups of cousins, the Kauravas (sons of Dhritarashtra, the descendant of Kuru) and the Pandavas (sons of Pandu). The poem is made up of almost 100,000 couplets—about seven times the length of the Iliad and the Odyssey combined—divided into 18 parvans, or sections, plus a supplement titled Harivamsha (“Genealogy of the God Hari”; i.e., of Vishnu). Although it is unlikely that any single person wrote the poem, its authorship is traditionally ascribed to the sage Vyasa, who appears in the work as the grandfather of the Kauravas and the Pandavas. The date and even the historical occurrence of the war that is the central event of the Mahabharata are much debated."    },
-        buylink:"#"
+        buylink:"https://www.amazon.in/Mahabharata-C-Rajagopalachari/dp/8172764766"
     }
 ];
 export default epics;
